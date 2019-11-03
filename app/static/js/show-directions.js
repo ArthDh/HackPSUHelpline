7fef43d5aa2de6df6fd404756933cd237ef326a3
arth <arth.dharaskar97@gmail.com>
2019-11-03 13:16:43 -0500

    Directions functions

---
+++ app/static/js/show-directions.js
@@ -0,0 +1,33 @@
+
+function get_direction(destination){
+
+
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsRenderer = new google.maps.DirectionsRenderer({
+    draggable: false,
+    map: map,
+  });
+
+  directionsRenderer.addListener('directions_changed', function() {
+    computeTotalDistance(directionsRenderer.getDirections());
+  });
+
+  return displayRoute(origin, destination, directionsService,
+      directionsRenderer);
+}
+
+function displayRoute(origin, destination, service, display) {
+  service.route({
+    origin: origin,
+    destination: destination,
+    travelMode: 'DRIVING',
+    avoidTolls: true
+  }, function(response, status) {
+    if (status === 'OK') {
+      display.setDirections(response);
+    } else {
+      alert('Could not display directions due to: ' + status);
+    }
+  });
+}
